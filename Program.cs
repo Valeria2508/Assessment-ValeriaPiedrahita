dd5d560a257562dc3691e0e6e271ce9eb6dfba74
val <valeria@gmail.com>
2024-11-15 09:50:46 -0500

    feat: proyect configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using Assessment_ValeriaPiedrahita.data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//config db
+Env.Load();
+
+var db_host = Environment.GetEnvironmentVariable("DB_HOST");
+var db_port = Environment.GetEnvironmentVariable("DB_PORT");
+var db_database = Environment.GetEnvironmentVariable("DB_DATABASE");
+var db_username = Environment.GetEnvironmentVariable("DB_USERNAME");
+var db_password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var conectionDb = $"server={db_host};port={db_port};database={db_database};uid={db_username};password={db_password}";
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(conectionDb, ServerVersion.Parse("8.0.20-mysql")));
 
 // Add services to the container.
 
